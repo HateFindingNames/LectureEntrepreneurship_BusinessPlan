717a033cc72d3a0baede846a1695065b219933fa
HateFindingNames <52651426+HateFindingNames@users.noreply.github.com>
2024-08-27 08:41:05 +0200

    eprice plot update

--- stat/EnergyPriceDev/plot.py
+++ stat/EnergyPriceDev/plot.py
@@ -6,7 +6,10 @@ import matplotlib.pyplot as plt
 efile = "\\elektr_nhk_ab-2007.tsv"
 gfile = "\\gas_nhk_ab-2007.tsv"
 
-# heads = ["Freq", "Product"]
+I_TAX = True
+X_TAX = False
+I_VAC = False
+X_VAC = False
 
 class data_dict(dict):
     "https://stackoverflow.com/a/57006277"
@@ -24,22 +27,23 @@ with open(os.path.dirname(__file__)+efile, "r") as e:
     for i, line in enumerate(e.readlines()):
         line = line.strip()
         line = line.split(sep="\t")
-        line = [x.strip().replace(" e", "").replace(" p", "") for x in line]
+        line = [x.strip().replace(" e", "").replace(" p", "").replace(" d", "").replace(" c", "") for x in line]
         line[0] = line[0].split(sep=",")
         if i == 0:
             heads = line
             properties = heads[0]
             properties[-1] = "geo"
             continue
-        # elif i == 1:
-        #     print(properties)
-        #     print(line)
-        ctry.key = line[0][-1]
-        ctry.value = line[1:]
-        ctry.add(ctry.key, ctry.value)
+        elif line[0][5] == "EUR" and line[0][4] == "X_TAX" and line[0][3] == "KWH" and line[0][2] == "MWH20-499":
+            # print(line)
+            ctry.key = line[0][-1]
+            print(line[0][-1])
+            ctry.value = line[1:]
+            print(line[1:])
+            ctry.add(ctry.key, ctry.value)
     
 edf = pd.DataFrame.from_dict(ctry, dtype="float")
-# print(edf)
+print(edf)
 # print(heads)
 
 xlabel = "Zeitperioden"
@@ -47,7 +51,15 @@ ylabel = "Euro pro kWh"
 title = "Titel"
 fig, ax = plt.subplots(1,1,figsize=(10, 5))
 
-countries = edf.columns
+# countries = edf.columns
+countries = [
+    "DE",
+    "ES",
+    "UK",
+    "FR",
+    "IT",
+    "AT"
+]
 for country in countries:
     ax.plot(heads[1:], edf[country], "-o", markersize=.5, label=country)
 ax.legend()
